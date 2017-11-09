dd5b872baea1cecb93f6062ac30d92edf5d9e867
Whitney Reans <wreans@gmail.com>
2017-11-09 16:11:07 -0600

    Remove console.log

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,6 @@ $(document).ready(function() {
     });
 
     particlesJS.load('particles-js', 'assets/particles.json', function() {
-        console.log('callback - particles.js config loaded');
+
     });
 });
